c2afda8c7043d37038b9d387eefe5665ac7cca6c
Matt <ballofmatts@gmail.com>
2016-12-06 08:18:03 -0600

    Adding flush to debug output

--- includes/common.php
+++ includes/common.php
@@ -7,4 +7,6 @@ function dbg($value,$color = 'yellow')
     echo '<div style="border:1px solid black; background-color:'.$color.'"><pre>';
     print_r($value);
     echo '</pre></div>';
+    flush();
+    ob_flush();
 }
