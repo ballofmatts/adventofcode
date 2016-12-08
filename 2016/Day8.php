1a25a9e1ad5de2f3f1aebb80c3630e3f1b9580d2
Matt <ballofmatts@gmail.com>
2016-12-08 10:59:09 -0600

    Day 8 solution

---
+++ 2016/Day8.php
@@ -0,0 +1,56 @@
+<?php
+include('../includes/common.php');
+$CommandFile = file_get_contents('./8.dat', true);
+
+$commands = explode("\n", $CommandFile);
+
+//$commands = array('rect 3x2','rotate column x=1 by 1','rotate row y=0 by 4','rotate column x=1 by 1');
+$grid = array_fill(0, 6, array_fill(0, 50, 0));
+
+//$grid = array_fill(0, 3, array_fill(0, 7, 0));
+function dispGrid($grid) {
+    echo '<pre>';
+    foreach($grid as $row) {
+        foreach($row as $col) {
+            echo ($col==1?'#':' ').' ';
+        }
+        echo "\n";
+    }
+    echo "\n";
+    echo '</pre>';
+}
+
+foreach($commands as $command) {
+    if(substr($IP,0,4) == 'rect') {
+        preg_match('/(\d+)x(\d+)/', $command, $matches);
+        for($i=0;$i<$matches[1];$i++) {
+            for($j=0;$j<$matches[2];$j++) {
+                $grid[$j][$i] = 1;
+            }
+        }
+    }
+    else {
+        preg_match('/([xy])=(\d+) by (\d+)/', $command, $matches);
+        if($matches[1] == 'y') {
+            for($i=0;$i<$matches[3];$i++) {
+                array_unshift($grid[$matches[2]], array_pop($grid[$matches[2]]));
+            }
+        }
+        else {  //x
+            $tempGrid = array_map(function($element){global $matches;return $element[$matches[2]];}, $grid);
+            for($i=0;$i<$matches[3];$i++) {
+                array_unshift($tempGrid, array_pop($tempGrid));
+            }
+            foreach($tempGrid as $key=>$val) {
+                $grid[$key][$matches[2]] = $val;
+            }
+        }
+    }
+}
+
+foreach($grid as $row) {
+    $totalPixels += array_sum($row);
+}
+
+dbg('Total P1 TLS IPs: '.$totalPixels,'lightgreen');
+dispGrid($grid);
